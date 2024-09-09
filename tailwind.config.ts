55b700930285741303ee01fe38c20eb77635228f
arda tor <arda.babathor@gmail.com>
2024-09-09 14:15:15 +0200

    refactor: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,20 @@ const config: Config = {
       colors: {
         background: "#F9F5F2",
         primarycolor: "#F7CB45",
-        secondry:"#1F1F1F",
+        secondry: "#1F1F1F",
+      },
+      screens: {
+        sm: '567px',
+        md: '768px',
+        lg: '992px',
+        xl: '1200px',
+        '2xl': '1440px',
+        '3xl': '1920px',
       },
     },
   },
   plugins: [],
 };
+
 export default config;
+
