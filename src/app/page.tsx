a911c5c8b83394e99205d4ca43aca7eeb9a9a318
arda tor <arda.babathor@gmail.com>
2024-09-10 10:49:35 +0200

    feat:add aboutus page and small improvement in folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-import Image from "next/image";
+import React from 'react';
+import Header from '../components/layout/header/Header';
+import '../app/globals.css';
 
-export default function Home() {
-  return (
-    <div/>
-  );
-}
+export default function Homepage(): JSX.Element {
+    return (
+        <div className="min-h-screen text-foreground">
+            
+            <main className="p-4">
+                <h2>Welcome to the Homepage</h2>
+                <p>This is the main content of the homepage.</p>
+            </main>
+        </div>
+    );
+};
